f749ae0c1b13bec5a25d0d929a7b146a0df2b3e7
Hugo Sobral <hugosobral10@gmail.com>
2020-06-29 22:21:58 -0300

    Add initial structure of control bar in VideoPlayer
    Co-authored-by: Micaella Gouveia <micaella2212@gmail.com>

--- src/components/VideoPlayer/index.js
+++ src/components/VideoPlayer/index.js
@@ -1,22 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import { Video } from "expo-av";
+import { View } from "react-native";
+import { MaterialIcons, Octicons } from "@expo/vector-icons";
 
 import styles from "./styles";
 
 export default function VideoPlayer() {
+  const [shouldPlay, setShoulPlay] = useState(true);
+  const [mute, setMute] = useState(false);
+
   return (
-    <Video
-      source={{
-        uri: "http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
-      }}
-      rate={1.0}
-      volume={1.0}
-      isMuted={false}
-      resizeMode="cover"
-      shouldPlay={false}
-      isLooping={false}
-      useNativeControls
-      style={styles.video}
-    />
+    <>
+      <Video
+        source={{
+          uri: "http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
+        }}
+        rate={1.0}
+        volume={1.0}
+        isMuted={false}
+        resizeMode="cover"
+        shouldPlay={shouldPlay}
+        isLooping={false}
+        // useNativeControls
+        style={styles.video}
+      />
+      <View style={styles.controlBar}>
+        <MaterialIcons
+          name={mute ? "volume-mute" : "volume-up"}
+          size={45}
+          color="white"
+          onPress={() => {
+            setMute(!mute);
+          }}
+        />
+        <MaterialIcons
+          name={shouldPlay ? "pause" : "play-arrow"}
+          size={45}
+          color="white"
+          onPress={() => {
+            setShoulPlay(!shouldPlay);
+          }}
+        />
+      </View>
+    </>
   );
 }
--- src/components/VideoPlayer/styles.js
+++ src/components/VideoPlayer/styles.js
@@ -7,4 +7,15 @@ export default StyleSheet.create({
     width: width - 18,
     height: height / 3,
   },
+  controlBar: {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    height: 45,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
 });
